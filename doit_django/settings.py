ab52dd276cee2367d728a11d7821b1fdd4257c27
kyongha kim <magpia001@gmail.com>
2022-10-20 20:12:07 +0900

    blog앱과 single_pages 앱 생성하기

--- doit_django/settings.py
+++ doit_django/settings.py
@@ -14,7 +14,7 @@ https://ca.ramel.be/67
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
