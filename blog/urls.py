7d226771478b25cf265dc71fdc13c0c0102b047a
kyongha kim <magpia001@gmail.com>
2022-10-21 01:22:34 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     # 도메인/blog/
     path('', views.index),
+    
     # 도메인/blog/1/
+    path('<int:pk>/', views.single_post_page),
+    
     # 도메인/about_me/
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,12 @@ from .models import Post
 
 # Create your views here.
 def index(request):
+    # 내림차순 정렬
     posts = Post.objects.all().order_by('-pk')
 
     return render(request,'blog/index.html', {'posts':posts})
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(request, 'blog/single_post_page.html', {'post':post})
