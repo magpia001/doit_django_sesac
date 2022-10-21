a1abda336627c252e15b8ba9467601396db6c325
kyongha kim <magpia001@gmail.com>
2022-10-22 01:08:32 +0900

    포스트 리스트, 상세 페이지에 대표 이미지 출력하기

--- doit_django/settings.py
+++ doit_django/settings.py
@@ -132,9 +132,12 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, '.static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
