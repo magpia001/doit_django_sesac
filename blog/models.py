96211cb9f16b4b7c505c99564f42e6cf00bf15e5
kyongha kim <magpia001@gmail.com>
2022-10-23 19:12:10 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -10,6 +11,7 @@ class Post(models.Model):
     # created_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'[{self.pk}] - {self.title}'
